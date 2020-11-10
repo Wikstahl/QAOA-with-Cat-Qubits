2ecfc32eaa5121b6250c36d7d1a41067d88d1757
Pontus Wikståhl <vikstal@chalmers.se>
2020-11-10 13:02:53 +0100

    :wrench: updated circuit simulator

--- circuit-simulator.py
+++ circuit-simulator.py
@@ -13,7 +13,7 @@ from ptm.ptm import *
 
 
 rho_vec = qload('results/rho_vec') # initial state
-for l in range(3,10):
+for l in range(1,10):
     filename = "instance_8_"+str(l)
     mat = spio.loadmat("instances/" + filename + ".mat")
     instance = mat['instance']
@@ -21,16 +21,13 @@ for l in range(3,10):
     graph = instance['graph'][0,0]
     J = graph.todense()/2
     # Iteration level
-    p = 1
-    gamma = instance['gamma'][0,0][0][p-1].flatten()
+    p = 2
     beta = instance['beta'][0,0][0][p-1].flatten()
     cv_ptm = PTM()
-
-    temp2 = cv_ptm.rx(2*beta)
-    temp1 = qload("results/"+filename+"_carb_p_1_1")
-
     U = 1
     for i in range(p):
+        temp1 = qload("results/"+filename+"_carb_p_"+str(i+1)+"of"+str(p))
+        temp2 = cv_ptm.rx(2*beta[i])
         for j in range(N-1):
             for k in range(j+1,N):
                 if J[j,k] != 0.:
@@ -38,5 +35,5 @@ for l in range(3,10):
         for j in range(N):
             U = ptm_expand_1toN(temp2, N=N, target=j) * U
     rho_final = pauli_basis_to_rho(U*rho_vec)
-    qsave(rho_final,"results/"+filename+"_rho_final_p_1")
+    qsave(rho_final,"results/"+filename+"_rho_final_p_"+str(p))
     print("save complete")
