f34b829affafd78451ff2ea172de67c7a61014ca
Wikstahl <pontus.wikstahl@gmail.com>
2020-12-21 16:09:37 +0100

    Restructed the working directory and made it into a python package.
    Moved all the figs, instances  and results to its own repository.

---
+++ cvqaoa/__init__.py
@@ -0,0 +1,9 @@
+
+# -----------------------------------------------------------------------------
+# Load modules
+#
+
+# core
+from cvqaoa.ptm import *
+from cvqaoa.cvdevice import *
+from cvqaoa.dvdevice import *
