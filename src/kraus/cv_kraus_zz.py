e6e95ca8957ef73b5c1b660003caea8d22b5f310
Wikstahl <pontus.wikstahl@gmail.com>
2022-01-31 14:45:33 +0100

    Update cv_kraus_zz.py
    Retrive the kraus operators

--- src/kraus/cv_kraus_zz.py
+++ src/kraus/cv_kraus_zz.py
@@ -6,8 +6,21 @@ from qutip.qip.circuit import QubitCircuit
 from cvqaoa.cvdevice import KPOProcessor
 from cvqaoa.gates import carb
 
+
+def product(*args, repeat=1):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x + [y] for x in result for y in pool]
+    for prod in result:
+        yield list(prod)
+
+
 # KPO parameters
-kpo = KPOProcessor(N=2, num_lvl=20)
+N = 2  # number of qubits
+kpo = KPOProcessor(N=N, num_lvl=20)
 alpha = kpo._paras['Coherent state']
 num_lvl = kpo._paras['Cut off']
 
@@ -29,63 +42,63 @@ sigma_x = up * down.dag() + down * up.dag()
 # sigma y
 sigma_y = 1j * (-up * down.dag() + down * up.dag())
 # List with Pauli matrices
-sigma = [I, sigma_x, sigma_y, sigma_z]
-# qubit Paulis
-P = [qeye(2), sigmax(), sigmay(), sigmaz()]
+sigma = list(
+    map(tensor, product([I, sigma_x, sigma_y, sigma_z], repeat=2)))
+
+# Create all qubit paulis
+P = list(
+    map(tensor, product([qeye(2), sigmax(), sigmay(), sigmaz()], repeat=2)))
 
 # Dimension
-d = 4
+d = 2**N
 
 # Pauli transfer matrix
 R = np.zeros((d**2, d**2))
 R_inv = np.zeros((d**2, d**2))  # Inverse
 
 # Quantum circuit
-qc = QubitCircuit(N=2)
+qc = QubitCircuit(N=N)
 qc.user_gates = {"CARB": carb}
-qc.add_gate("CARB", 0, None, 0)
+qc.add_gate("CARB", targets=[0, 1], arg_value=0)
 
-for i in range(d):
-    for j in range(d):
-        sigma_ij = tensor(sigma[i], sigma[j])
-        result = kpo.run_state(init_state=sigma_ij, qc=qc, noisy=True)
-        result_inv = kpo.run_state(init_state=sigma_ij, qc=qc, noisy=False)
+for j in range(d**2):
+    result = kpo.run_state(init_state=sigma[j], qc=qc, noisy=True)
+    result_inv = kpo.run_state(init_state=sigma[j], qc=qc, noisy=False)
 
-        Lambda = result.states[-1]
-        Lambda_inv = result_inv.states[-1]
+    Lambda = result.states[-1]
+    Lambda_inv = result_inv.states[-1]
 
-        for k in range(d):
-            for l in range(d):
-                sigma_kl = tensor(sigma[k], sigma[l])
-                R[(k + l), (i + j)] = 1 / d * ((sigma_kl * Lambda).tr()).real
-                R_inv[(k + l), (i + j)] = 1 / d * \
-                    ((sigma_kl * Lambda_inv).tr()).real
+    for i in range(d**2):
+        R[i, j] = 1 / d * ((sigma[i] * Lambda).tr()).real
+        R_inv[i, j] = 1 / d * ((sigma[i] * Lambda_inv).tr()).real
 
-# Get the PTM error channel
+# Find the PTM of the error channel
 ptm_error = R@np.linalg.inv(R_inv)
-
 # Convert PTM to choi
-rho = np.zeros((d**2, d**2))
-for i in range(d):
-    for j in range(d):
-        P_ij = tensor(P[i], P[j])
-        for k in range(d):
-            for l in range(d):
-                rho[(i + j), (k + l)] = 1 / d**2 * \
-                    sum(ptm_error[(i + j), (k + l)]
-                        * tensor(P_ij.trans(), P_kl))
+choi = 1 / d**2 * sum((ptm_error[i, j] * tensor(P[j].trans(), P[i]))
+                      for i in range(d**2) for j in range(d**2))
+choi.dims = [[[2, 2], [2, 2]], [[2, 2], [2, 2]]]
+choi.superrep = 'choi'
 
 choi = Qobj(rho, dims=[[[2, 2], [2, 2]],
                        [[2, 2], [2, 2]]], superrep='choi')
 
-# Convert choi to kraus. Each kraus needs to be scaled by a factor sqrt(d)
-kraus = [np.sqrt(d) * k for k in choi_to_kraus(choi)]
+# Find eigenvectors and eigenvalues to choi
+eValues, eVectors = np.linalg.eigh(choi)
+# Because of machine impressision we drop terms smaller than rtol
+rtol = 1e-6
+idx, = np.where(eValues < rtol)
+eVectors = np.delete(eVectors, idx, axis=1)  # drop columns
+eValues = np.delete(eValues, idx)
+num = len(eValues)
+# Get the Kraus operators
+kraus = [np.sqrt(d * eValues[i]) * eVectors[:, i].reshape((d, d))
+         for i in range(num)]
 
-# Identity
-id = sum(k * k.dag() for k in kraus)
-
-# Check that the kraus sum to identity
-if np.isclose(id.full(), qeye(4).full(), rtol=1e-5, atol=1e-5).all() != True:
+# identity
+id = sum(k@np.conj(k.T) for k in kraus)
+# check that the kraus sum to identity
+if np.isclose(id, np.eye(4), rtol=1e-3, atol=1e-3).all() != True:
     raise 'Kraus operators must sum to identity'
 
 # Save results
