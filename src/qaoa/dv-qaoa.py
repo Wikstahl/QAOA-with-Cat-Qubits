11c1b9cda9a3a0d20e0d819a27c44f479fdf5914
Wikstahl <pontus.wikstahl@gmail.com>
2022-02-07 14:22:13 +0100

    created qaoa files

---
+++ src/qaoa/dv-qaoa.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+import networkx as nx
+from scipy.optimize import brute
+from cvqaoa.circ import Circ
+
+# Loop over all instances
+for idx in range(30):
+    # Path
+    path = "../../data/instances/max_cut_"+str(idx)+"/"
+    # Load graph
+    with open(path + "graph", 'rb') as pickle_file:
+        graph = pickle.load(pickle_file)
+    # Create object
+    circ = Circ(graph)
+    # Optimization bounds
+    ranges = ((0, np.pi), (0, np.pi/2))
+    # Brute force on 100 x 100 grid
+    res = brute(circ.optimize_qaoa, ranges, args=(["DV"]), Ns=100,
+                full_output=True, finish=None, workers=-1)
+    # Save results
+    filename = path + "qaoa_parameters_dv"
+    with open(filename, 'wb') as f:
+        pickle.dump(res, f)
